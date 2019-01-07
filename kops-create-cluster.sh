a9ecd0415e45f9b603d845afbcf6620403508c2c
sujeet_kumar <sujeet.kumar@fleetcomplete.com>
2019-01-06 22:00:35 -0500

    added taking influxdb backup to s3 on cluster delete and config file for shell script

--- kops-create-cluster.sh
+++ kops-create-cluster.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/env bash
 
-KOPS_CLUSTER_NAME=nameOfCluster.k8s.local
-AWS_REGION=us-east-2
-AWS_AVAILABILITY_ZONE=us-east-2a
+source ./perf-load-gen.cfg
 
-EBS_TAG_TMP_VAL="Tags=[{Key=KubernetesCluster,Value=$KOPS_CLUSTER_NAME},{Key=PVPerfTesting,Value=InfluxDBPV}]"
+EBS_TAG_TMP_VAL="Tags=[{Key=Name,Value=Sujeet-PerfTest-InfluxDBPV},{Key=KubernetesCluster,Value=$KOPS_CLUSTER_NAME},{Key=PVPerfTesting,Value=InfluxDBPV}]"
 
 #TODO  check kops installation before trying to create the cluster
 
-kops create -f $KOPS_CLUSTER_NAME.yaml
-kops create secret --name $KOPS_CLUSTER_NAME sshpublickey admin -i ~/.ssh/id_rsa.pub
-kops update cluster --name=$KOPS_CLUSTER_NAME --yes
-kops rolling-update cluster $KOPS_CLUSTER_NAME --yes
+# kops create -f $KOPS_CLUSTER_NAME.yaml
+kops create secret --name ${KOPS_CLUSTER_NAME} sshpublickey admin -i ~/.ssh/id_rsa.pub
+kops replace --force -f ${KOPS_CLUSTER_NAME}.yaml
+kops update cluster --name=${KOPS_CLUSTER_NAME} --yes
+kops rolling-update cluster ${KOPS_CLUSTER_NAME} --yes
 
 #Creating the EBS Persistent Volume
 echo
 echo "Creating EBS Persistent Volume"
-eval aws ec2 create-volume --size 10 --region $AWS_REGION --availability-zone $AWS_AVAILABILITY_ZONE --volume-type gp2 --tag-specifications 'ResourceType=volume,$EBS_TAG_TMP_VAL'
+eval aws ec2 create-volume --size 10 --region ${AWS_REGION} --availability-zone ${AWS_AVAILABILITY_ZONE} --volume-type gp2 --tag-specifications 'ResourceType=volume,${EBS_TAG_TMP_VAL}'
+
+cluster_status=`kops validate cluster | grep "Your cluster ${KOPS_CLUSTER_NAME} is ready"`
+
+while [ "${cluster_status}" != "Your cluster ${KOPS_CLUSTER_NAME} is ready" ]; do
+   sleep 20s
+   echo "Waiting for Cluster nodes to be ready ..."
+   cluster_status=`kops validate cluster | grep "Your cluster ${KOPS_CLUSTER_NAME} is ready"`
+done
+
+echo
+echo
+echo "All Cluster Nodes are ready"
+echo
+echo
+echo `kops validate cluster`
--- kops-delete-cluster.sh
+++ kops-delete-cluster.sh
@@ -2,8 +2,31 @@
 
 #TODO check kops installation before trying to delete cluster
 
-bucket_name=sujeet-pt-kops-state-store
-KOPS_CLUSTER_NAME=sujeet.k8s.local
-KOPS_STATE_STORE=s3://${bucket_name}
+working_dir=`pwd`
+source ./perf-load-gen.cfg
 
-kops delete cluster --name=${KOPS_CLUSTER_NAME} --state=${KOPS_STATE_STORE} --yes
+if [ -z $1 ]
+then
+  kops delete cluster --name=${KOPS_CLUSTER_NAME} --state=${KOPS_STATE_STORE} --yes
+else
+  influxdb_pod=`kubectl get po -n ${thenamespace} | grep influxdb-jmeter | awk '{print $1}'`
+  echo
+  echo "Creating a Back up of InfluxDB to /tmp "
+  kubectl exec -it -n ${thenamespace} ${influxdb_pod} -- rm -rf ${BACKUP_FOLDER}${BACKUP_NAME}
+  kubectl exec -it -n ${thenamespace} ${influxdb_pod} -- rm -f ${BACKUP_ARCHIVE_LOCATION}
+  kubectl exec -it -n ${thenamespace} ${influxdb_pod} -- mkdir ${BACKUP_FOLDER}${BACKUP_NAME}
+  kubectl exec -it -n ${thenamespace} ${influxdb_pod} -- influxd backup -portable -database ${DATABASE_NAME} ${BACKUP_FOLDER}${BACKUP_NAME}
+  kubectl exec -it -n ${thenamespace} ${influxdb_pod} -- tar -cvzf ${BACKUP_ARCHIVE_LOCATION} ${BACKUP_FOLDER}${BACKUP_NAME}
+  kubectl exec -it -n ${thenamespace} ${influxdb_pod} -- aws s3 cp ${BACKUP_ARCHIVE_LOCATION} s3://${S3_BUCKET}/${BACKUP_ARCHIVE_NAME}
+
+  sleep 2s
+
+    # if aws s3 cp $BACKUP_ARCHIVE_PATH s3://${S3_BUCKET}/${S3_KEY_PREFIX}latest.tgz; then
+    #     echo "Backup file copied to s3://${S3_BUCKET}/${S3_KEY_PREFIX}latest.tgz"
+    #   else
+    #     echo "Backup file failed to upload"
+    #     exit 1
+    #   fi
+  kops delete cluster --name=${KOPS_CLUSTER_NAME} --state=${KOPS_STATE_STORE} --yes
+
+fi
